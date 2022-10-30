e906d7e9599e37ad868bd73e002d0ed44cdca0b6
MichalZmu <michal.zmuda.mail@gmail.com>
2022-10-30 20:21:22 +0100

    state changes

--- src/app/states/tasks/tasks.reducer.ts
+++ src/app/states/tasks/tasks.reducer.ts
@@ -2,7 +2,6 @@ import { createReducer, on, State } from '@ngrx/store';
 import * as tasksActions from './tasks.actions';
 import { AppState } from '../app.state';
 
-const FORM_ID = 'some globally unique string';
 
 export const initialState: AppState = {
     tasks: JSON.parse(localStorage.getItem('tasks')),
@@ -10,24 +9,22 @@ export const initialState: AppState = {
 
 export const TasksReducer = createReducer(
     initialState,
-    on(tasksActions.getTasks, (state) => {
-        return { tasks: state.tasks };
-    }),
     on(tasksActions.addTask, (state, action) => {
-        return { tasks: [...state.tasks, action.task] };
+        return { ...state, tasks: [...state.tasks, action.task] };
     }),
     on(tasksActions.updateTask, (state, action) => {
         console.log('action: ', action);
         console.log('state: ', state);
 
         return {
+            ...state,
             tasks: state.tasks.map((value) =>
                 value._id === action.task._id ? (value = action.task) : value
             ),
         };
     }),
     on(tasksActions.setTasks, (state, action) => {
-        return { tasks: [...state.tasks, ...action.tasks] };
+        return { ...state, tasks: [...action.tasks] };
     }),
     on(tasksActions.deleteTask, (state, action) => {
         console.log('action: ', action);
@@ -39,8 +36,6 @@ export const TasksReducer = createReducer(
             taskList.splice(index, 1); // 2nd parameter means remove one item only
         }
 
-        return {
-            tasks: taskList,
-        };
+        return { ...state, tasks: taskList };
     })
 );
