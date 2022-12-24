192e4600309490ee389520856c28a0ccdba50a36
MichalZmu <michal.zmuda.mail@gmail.com>
2022-12-24 16:23:27 +0100

    update status fix

--- src/app/components/task-details/task-details.component.ts
+++ src/app/components/task-details/task-details.component.ts
@@ -11,52 +11,55 @@ import { AppState } from '../../states/app.state';
 import { deleteTask, updateTask } from '../../states/tasks/tasks.actions';
 
 @Component({
-    selector: 'app-task-details',
-    templateUrl: './task-details.component.html',
-    styleUrls: ['./task-details.component.scss'],
+  selector: 'app-task-details',
+  templateUrl: './task-details.component.html',
+  styleUrls: ['./task-details.component.scss'],
 })
 export class TaskDetailsComponent implements OnInit {
-    task: TaskItemModel;
-    readOnly = true;
-    readonly statuses = Object.values(TaskStatuses).slice(0, 3);
+  task: TaskItemModel;
+  readOnly = true;
+  readonly statuses = Object.values(TaskStatuses).slice(0, 3);
 
-    constructor(
-        private activatedRoute: ActivatedRoute,
-        private taskService: TaskService,
-        private navigationService: NavigationService,
-        private router: Router,
-        private store: Store<AppState>
-    ) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private taskService: TaskService,
+    private navigationService: NavigationService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {
+  }
 
-    ngOnInit(): void {
-        this.navigationService.setGoBackUrl('/');
+  ngOnInit(): void {
+    this.navigationService.setGoBackUrl('/');
 
-        this.activatedRoute.queryParams
-            .pipe(switchMap((params) => this.getTask(params.taskId)))
-            .subscribe((data: TaskItemModel) => {
-                this.task = data;
-            });
+    this.activatedRoute.queryParams
+      .pipe(switchMap((params) => this.getTask(params.taskId)))
+      .subscribe((data: TaskItemModel) => {
+        this.task = data;
+      });
+  }
+
+  getTask(id: string): any {
+    return this.taskService.getTask(id);
+  }
+
+  editData(flag: boolean): void {
+    this.readOnly = !flag;
+  }
+
+  updateData(readOnly = true): void {
+    this.taskService.updateTask(this.task).subscribe(() => {
+      this.store.dispatch(updateTask({task: this.task}));
+    });
+    if (readOnly) {
+      this.readOnly = true;
     }
+  }
 
-    getTask(id: string): any {
-        return this.taskService.getTask(id);
-    }
-
-    editData(flag: boolean): void {
-        this.readOnly = !flag;
-    }
-
-    updateData(): void {
-        this.taskService.updateTask(this.task).subscribe(() => {
-          this.store.dispatch(updateTask({task: this.task}));
-        });
-        this.readOnly = true;
-    }
-
-    deleteData(): void {
-        this.taskService.deleteTask(this.task).subscribe((data) => {
-            this.store.dispatch(deleteTask({ task: this.task }));
-            this.router.navigateByUrl('/').then();
-        });
-    }
+  deleteData(): void {
+    this.taskService.deleteTask(this.task).subscribe((data) => {
+      this.store.dispatch(deleteTask({task: this.task}));
+      this.router.navigateByUrl('/').then();
+    });
+  }
 }
